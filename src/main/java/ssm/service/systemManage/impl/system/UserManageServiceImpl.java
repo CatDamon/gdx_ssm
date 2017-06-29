e3a3edbc3a091389ca2963f909f552d95a510d6b
CatDamon <dxgong1994313@163.com>
2017-06-29 10:49:59 +0800

    添加添加用户业务层方法

--- src/main/java/ssm/service/systemManage/impl/system/UserManageServiceImpl.java
+++ src/main/java/ssm/service/systemManage/impl/system/UserManageServiceImpl.java
@@ -1,9 +1,11 @@
 package ssm.service.systemManage.impl.system;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import ssm.service.common.impl.BaseServiceImpl;
 import ssm.service.systemManage.system.UserManageService;
 import ssm.state.AvailableState;
+import ssm.utils.DES;
 import ssm.utils.Page;
 import ssm.utils.PageData;
 
@@ -24,5 +26,19 @@ public class UserManageServiceImpl extends BaseServiceImpl implements UserManage
             userData.put("available", AvailableState.codeToDesc(userData.get("available").toString()));
         }
         return userList;
+    }
+
+    /**
+     * 添加用户
+     */
+    @Override
+    public void saveUser(PageData pageData) throws Exception {
+        logger.info("UserManageServiceImpl saveUser...");
+        //密码DES加密
+        if(StringUtils.isNotBlank(pageData.getString("password"))){
+            pageData.put("password", DES.get3DESEncrypt(pageData.getString("password")));
+        }
+        this.daoSupport.save("",pageData);
+
     }
 }
